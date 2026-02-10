dd9b967144c526caafb01467836cd8fa285c8852
CristianPinedaD <alternativec141@gmail.com>
2026-02-10 12:36:18 -0500

    added save/restore functionality and refactored some code that was redundant

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, { useRef, useCallback, useState } from 'react';
-import ReactFlow, { ReactFlowProvider, MarkerType, useReactFlow, addEdge, applyEdgeChanges, applyNodeChanges, Background, Controls } from 'reactflow';
-import 'reactflow/dist/style.css';
+import React, { useRef, useCallback, useState } from "react";
+import ReactFlow, {
+  ReactFlowProvider,
+  MarkerType,
+  useReactFlow,
+  addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
+  Background,
+  Controls,
+  useNodesState,
+  useEdgesState,
+  Panel,
+} from "reactflow";
+import "reactflow/dist/style.css";
 
 import { initialNodes } from "./nodes.jsx";
 import { initialEdges } from "./edges.jsx";
-import Sidebar from './Sidebar.jsx';
-import { DnDProvider, useDnD } from './DnDContext.jsx';
+import Sidebar from "./Sidebar.jsx";
+import { DnDProvider, useDnD } from "./DnDContext.jsx";
 
-import ImageNode from './ImageNode.jsx';
-import ContextMenu from './ContextMenu.jsx';
+import ImageNode from "./ImageNode.jsx";
+import ContextMenu from "./ContextMenu.jsx";
 
 let id = 0;
 const getId = () => `${id++}`;
@@ -18,62 +30,90 @@ const nodeTypes = {
 };
 
 const addEndMarker = (edge) => ({
-    ...edge,
-    markerEnd: {
-        type: MarkerType.Arrow,
-    },
+  ...edge,
+  markerEnd: {
+    type: MarkerType.Arrow,
+  },
 });
+
+const flowKey = 'saved-flow';
 
 const Flow = () => {
   const reactFlowWrapper = useRef(null);
-  const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const { screenToFlowPosition } = useReactFlow();
   const [type] = useDnD();
   const [menu, setMenu] = useState(null);
   const ref = useRef(null);
+  const [rfInstance, setRfInstance] = useState(null);
+  const { setViewport } = useReactFlow();
 
   const onDragOver = useCallback((event) => {
     event.preventDefault();
-    event.dataTransfer.dropEffect = 'move';
+    event.dataTransfer.dropEffect = "move";
   }, []);
 
-  const onDrop = useCallback((event) => {
-    event.preventDefault();
+  const onDrop = useCallback(
+    (event) => {
+      event.preventDefault();
 
-    // check if the dropped element is valid
-    if (!type) {
-      return;
-    }
+      // check if the dropped element is valid
+      if (!type) {
+        return;
+      }
 
-    // project was renamed to screenToFlowPosition
-    // and you don't need to subtract the reactFlowBounds.left/top anymore
-    // details: https://reactflow.dev/whats-new/2023-11-10
-    const position = screenToFlowPosition({ x: event.clientX, y: event.clientY });
-    const newNode = {
-      id: "dndNode_" + getId(),
-      type,
-      position,
-      data: { 
-        label: `${type} node`,
-        ...(type === 'image' && { image: {src: 'images/Satellite.png', height: 300, width: 400} }) // Add image to data if type is image
-      },
-    };
+      // project was renamed to screenToFlowPosition
+      // and you don't need to subtract the reactFlowBounds.left/top anymore
+      // details: https://reactflow.dev/whats-new/2023-11-10
+      const position = screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+      const newNode = {
+        id: "dndNode_" + getId(),
+        type,
+        position,
+        data: {
+          label: `${type} node`,
+          ...(type === "image" && {
+            image: { src: "images/Satellite.png", height: 300, width: 400 },
+          }), // Add image to data if type is image
+        },
+      };
 
-    setNodes((nds) => nds.concat(newNode));
-  }, [screenToFlowPosition, type]);
-
-  const onNodesChange = useCallback(
-    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)), [setNodes],
-  );
-  const onEdgesChange = useCallback(
-    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)), [setEdges],
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [screenToFlowPosition, type],
   );
   const onConnect = useCallback(
-    (connection) => setEdges((eds) => addEdge(addEndMarker(connection), eds)), [setEdges],
+    (connection) => setEdges((eds) => addEdge(addEndMarker(connection), eds)),
+    [setEdges],
   );
 
-  const onNodeContextMenu = useCallback((event, node) => {
+  const onSave = useCallback(() => {
+    if (rfInstance) {
+      const flow = rfInstance.toObject();
+      localStorage.setItem(flowKey, JSON.stringify(flow));
+    }
+  }, [rfInstance]);
+
+  const onRestore = useCallback(() => {
+    const restoreFlow = async () => {
+      const flow = JSON.parse(localStorage.getItem(flowKey));
+
+      if (flow) {
+        const { x = 0, y = 0, zoom = 1 } = flow.viewport;
+        setNodes(flow.nodes || []);
+        setEdges(flow.edges || []);
+        setViewport({ x, y, zoom });
+      }
+    };
+    restoreFlow();
+  }, [setNodes, setViewport]);
+
+  const onNodeContextMenu = useCallback(
+    (event, node) => {
       // Prevent native context menu from showing
       event.preventDefault();
 
@@ -81,11 +121,12 @@ const Flow = () => {
       // doesn't get positioned off-screen
       const pane = ref.current.getBoundingClientRect();
       setMenu({
-          id: node.id,
-          top: event.clientY < pane.height -200 && event.clientY,
-          left: event.clientX < pane.width - 200 && event.clientX,
-          right: event.clientX >= pane.width - 200 && pane.width - event.clientX,
-          bottom: event.clientY >= pane.height - 200 && pane.height - event.clientY,
+        id: node.id,
+        top: event.clientY < pane.height - 200 && event.clientY,
+        left: event.clientX < pane.width - 200 && event.clientX,
+        right: event.clientX >= pane.width - 200 && pane.width - event.clientX,
+        bottom:
+          event.clientY >= pane.height - 200 && pane.height - event.clientY,
       });
     },
     [setMenu],
@@ -94,43 +135,63 @@ const Flow = () => {
   // Close the context menu if it's open whenever the window is clicked.
   const onPaneclick = useCallback(() => setMenu(null), [setMenu]);
 
-  const duplicateNode = useCallback((id) => {
-    const node = nodes.find((node) => node.id === id);
-    const position = { x: node.position.x + 50, y: node.position.y + 50 };
+  const duplicateNode = useCallback(
+    (id) => {
+      const node = nodes.find((node) => node.id === id);
+      const position = { x: node.position.x + 50, y: node.position.y + 50 };
 
-    const newNode = {
-      ...node,
-      id: "duplicate_"+getId(),
-      position,
-    };
-    
-    setNodes((nds) => nds.concat(newNode));
-  }, [nodes]);
+      const newNode = {
+        ...node,
+        id: "duplicate_" + getId(),
+        position,
+      };
 
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [nodes],
+  );
 
   return (
-    <div className='dndflow'>
-      <div className='reactflow-wrapper' ref={reactFlowWrapper} style={{ height: '100%' }}>
-        <ReactFlow nodes={nodes} edges={edges}
+    <div className="dndflow">
+      <div
+        className="reactflow-wrapper"
+        ref={reactFlowWrapper}
+        style={{ height: "100%" }}
+      >
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
           ref={ref}
           onNodesChange={onNodesChange}
           onEdgesChange={onEdgesChange}
           onConnect={onConnect}
           onDrop={onDrop}
           onDragOver={onDragOver}
+          onInit={setRfInstance}
           onNodeContextMenu={onNodeContextMenu}
           onPaneClick={onPaneclick}
           nodeTypes={nodeTypes}
-          fitView>
+          fitView
+        >
           <Background />
-          {menu && <ContextMenu onClick={onPaneclick} duplicateNode={duplicateNode} {...menu} />}
+          <Panel position="top-right">
+            <button onClick={onSave}>save</button>
+            <button onClick={onRestore}>restore</button>
+          </Panel>
+          {menu && (
+            <ContextMenu
+              onClick={onPaneclick}
+              duplicateNode={duplicateNode}
+              {...menu}
+            />
+          )}
           <Controls />
         </ReactFlow>
       </div>
       <Sidebar />
     </div>
   );
-}
+};
 
 export default () => (
   <ReactFlowProvider>
