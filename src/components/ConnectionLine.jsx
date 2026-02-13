327ace9b3e2b98ff6db76913d52657d7346635a0
Cristian Pineda Delgado <cadelgado@sat.qc.ca>
2026-02-13 17:26:57 +0000

    eXes!!!!!

--- src/components/ConnectionLine.jsx
+++ src/components/ConnectionLine.jsx
@@ -7,25 +7,52 @@ export default ({ fromX, fromY, toX, toY, fromNode, toNode }) => {
 
   const isValid = toNode
     ? canConnect(fromNode.data.toolObj, toNode.data.toolObj)
-    : true; // allow dragging to empty space
+    : true;
+
+  const strokeColor = isValid ? fromHandle.id : "red";
+  const size = 4; // size of X arms
 
   return (
     <g>
       <path
         fill="none"
-        stroke={isValid ? fromHandle.id : "red"}
+        stroke={strokeColor}
         strokeWidth={1}
         className="animated"
         d={`M${fromX},${fromY} C ${fromX} ${toY} ${fromX} ${toY} ${toX},${toY}`}
       />
-      <circle
-        cx={toX}
-        cy={toY}
-        fill="#fff"
-        r={3}
-        stroke={fromHandle.id}
-        strokeWidth={1.5}
-      />
+
+      {/* End marker is a circle if it's valid */}
+      {isValid ? (
+        <circle
+          cx={toX}
+          cy={toY}
+          fill="#fff"
+          r={3}
+          stroke={strokeColor}
+          strokeWidth={1.5}
+        />
+      ) : (
+        <>
+          {/* End marker is an x if it's invalid */}
+          <line
+            x1={toX - size}
+            y1={toY - size}
+            x2={toX + size}
+            y2={toY + size}
+            stroke="red"
+            strokeWidth={1}
+          />
+          <line
+            x1={toX - size}
+            y1={toY + size}
+            x2={toX + size}
+            y2={toY - size}
+            stroke="red"
+            strokeWidth={1}
+          />
+        </>
+      )}
     </g>
   );
 };
