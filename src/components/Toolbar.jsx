5554d2b8904c394c097f21e9622c5b11896427a1
Sarah Al Mamoun <salmamoun@sat.qc.ca>
2026-02-22 03:17:51 +0000

    Resolve "Add logos and change SAT logo"

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -13,6 +13,36 @@ export function Toolbar(){
             <SidebarNode toolObj={tools.SatelliteObj} />
             <SidebarNode toolObj={tools.ScoreObj} />
             <SidebarNode toolObj={tools.SpatgrisObj} />
+            <SidebarNode toolObj={tools.KoaiaObj} />
+            <SidebarNode toolObj={tools.AbletonLiveObj} />
+            <SidebarNode toolObj={tools.ArdourObj} />
+            <SidebarNode toolObj={tools.BitwigObj} />
+            <SidebarNode toolObj={tools.BlenderObj} />
+            <SidebarNode toolObj={tools.ChataigneObj} />
+            <SidebarNode toolObj={tools.CinderObj} />
+            <SidebarNode toolObj={tools.CubaseObj} />
+            <SidebarNode toolObj={tools.IsadoraObj} />
+            <SidebarNode toolObj={tools.LeapMotionObj} />
+            <SidebarNode toolObj={tools.MadMapperObj} />
+            <SidebarNode toolObj={tools.MaxMSPObj} />
+            <SidebarNode toolObj={tools.NotchObj} />
+            <SidebarNode toolObj={tools.OBSStudioObj} />
+            <SidebarNode toolObj={tools.P5jsObj} />
+            <SidebarNode toolObj={tools.ProcessingObj} />
+            <SidebarNode toolObj={tools.PureDataObj} />
+            <SidebarNode toolObj={tools.ReaperObj} />
+            <SidebarNode toolObj={tools.ResolumeObj} />
+            <SidebarNode toolObj={tools.SmodeObj} />
+            <SidebarNode toolObj={tools.SplashObj} />
+            <SidebarNode toolObj={tools.SuperColliderObj} />
+            <SidebarNode toolObj={tools.TouchDesignerObj} />
+            <SidebarNode toolObj={tools.UnrealEngineObj} />
+            <SidebarNode toolObj={tools.VCVRackObj} />
+            <SidebarNode toolObj={tools.VDMXObj} />
+            <SidebarNode toolObj={tools.VezerObj} />
+            <SidebarNode toolObj={tools.VRChatObj} />
+            <SidebarNode toolObj={tools.VVVVObj} />
+            <SidebarNode toolObj={tools.WwiseObj} />
         </aside>
     )
 };
